c985e682f6c75f9ff1e341e46149c8beba8f2e0f
Thomas Ciepluch <tciepluch@hotmail.com>
2020-11-02 22:28:41 +0100

    make some changes to trigger build

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -12,6 +12,9 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+
+        // random change
+        
         private readonly ICommandAPIRepo _repository;
         private readonly IMapper _mapper;
 
